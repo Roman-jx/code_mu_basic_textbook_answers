11135fe28907061fa7038b56aec1991f8cb53928
Roman Malkevych <romanmalkevych@Romans-MacBook-Air.local>
2022-04-09 23:50:18 +0300

    -->

--- Forms/134_introduction_to_the_form/index.php
+++ Forms/134_introduction_to_the_form/index.php
@@ -1,5 +1,20 @@
-<form action="result.php" method="GET"> 
+// Task 1
+<form action="result.php" method="GET">
     <input name="text1">
     <input name="text2">
+    <input name="text3">
+    <input type="submit">
+</form>
+
+// Task 2
+<form action="result.php" method="POST">
+    Your name: <input type="text" name="name">
+    Your age:  <input type="number" name="age">
+    <input type="submit">
+</form>
+
+// Task 3
+<form action="result.php" method="POST">
+    Your password: <input type="password" name="pass">
     <input type="submit">
 </form>
--- Forms/134_introduction_to_the_form/result.php
+++ Forms/134_introduction_to_the_form/result.php
@@ -1,5 +1,9 @@
 <?php
-var_dump($_GET);
-var_dump($_POST);
-var_dump($_REQUEST);
+require_once "index.php";
+$pass = "12345";
+echo $_GET["text2"] + $_GET["text1"] + $_GET["text3"];
+
+echo $_POST["name"] . " " . $_POST["age"];
+
+echo "Your password is ".(bool)($_POST["pass"] == $pass);
 ?>
